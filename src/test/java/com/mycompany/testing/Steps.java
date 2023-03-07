62e523bf6a56bec3d3edd713e49a0a0b80afc85d
shashank <shashank.deepak@gmail.com>
2023-03-06 21:31:27 -0600

    commit msg

--- src/test/java/com/mycompany/testing/Steps.java
+++ src/test/java/com/mycompany/testing/Steps.java
@@ -19,55 +19,38 @@ public class Steps {
     int c;
     int sum;
 
-    @Дано("^первое число (\\d+)$")
+
+    @Given("^print$")
+    public void print() throws Throwable {
+        System.out.println("HELLO");;
+    }
+
     @Given("^first digit (\\d+)$")
     public void firstDigit(int digit) throws Throwable {
         a = digit;
     }
 
-    @Дано("^второе число (\\d+)$")
     @And("^second digit (\\d+)$")
     public void secondDigit(int digit) throws Throwable {
         b = digit;
     }
 
-    @Дано("^третье число (\\d+)$")
     public void thirdDigit(int digit) throws Throwable {
         c = digit;
     }
 
-    @Когда("^я их складываю$")
+
     @When("^I multiply it$")
     public void multiply() throws Throwable {
         sum = a + b + c;
     }
 
-    @Тогда("^сумма равна (\\d+)$")
     @Then("^sum is (\\d+)$")
     public void sum(int result) throws Throwable {
         Assert.assertEquals(result, sum);
     }
 
-    @Дано("^сломанный сценарий$")
-    public void brokenScenario() throws Throwable {
-        try {
-            Object o = 1;
-            String fail = (String) o;
-        } catch (Exception e) {
-            this.createAttachment(Arrays.toString(e.getStackTrace()));
-            throw e;
-        }
-    }
 
-    @Когда("^отображается отчет$")
-    public void seeTheReport() throws Throwable {
-
-    }
-
-    @То("^видно исключение$")
-    public void seeAnException() throws Throwable {
-
-    }
 
     @Attachment()
     private byte[] createAttachment(String att) {
---
+++ src/test/resources/test-allure_1.feature
@@ -0,0 +1,12 @@
+@en
+Feature: Test allure 1
+
+
+  Scenario: Test 1
+    Given print
+    Given print
+
+
+  Scenario: Test 2
+    Given print
+    Given print
